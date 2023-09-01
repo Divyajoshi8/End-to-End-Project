f982f01dd90bb79a2357312c414f0c4487bbca30
Divyajoshi8 <97658852+Divyajoshi8@users.noreply.github.com>
2023-09-01 12:51:11 +0530

    reset password link

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -11,33 +11,83 @@ categoryItems.forEach((item) => {
   });
 });
 
+
+
+// async function getLeaderboard() {
+//     clearLeaderboard();
+//     const res = await axios.get("http://localhost:3000/user/getAllUsers");
+//   let position = 1;
+//   console.log(res.data);
+//   res.data.forEach((user) => {
+//     let name = user.name;
+//     let amount = user.totalExpenses;
+
+//     //console.log(name, amount);
+//     let tr = document.createElement("tr");
+//     tr.setAttribute("class", "trStyle");
+
+//     tbody.appendChild(tr);
+
+//     let th = document.createElement("th");
+//     th.setAttribute("scope", "row");
+//     th.appendChild(document.createTextNode(position++));
+
+//     let td1 = document.createElement("td");
+//     td1.appendChild(document.createTextNode(name));
+
+//     let td2 = document.createElement("td");
+//     td2.appendChild(document.createTextNode(amount));
+
+//     tr.appendChild(th);
+//     tr.appendChild(td1);
+//     tr.appendChild(td2);
+//   });
+// }
 async function getLeaderboard() {
-  const res = await axios.get("http://localhost:3000/premium/getLeaderboard");
-  let position = 1;
-  res.data.forEach((user) => {
-    let name = user.name;
-    let amount = user.amount;
+    try {
+      
+  
+      const res = await axios.get("http://localhost:3000/user/getAllUsers");
 
-    console.log(name, amount);
-    let tr = document.createElement("tr");
-    tr.setAttribute("class", "trStyle");
-
-    tbody.appendChild(tr);
-
-    let th = document.createElement("th");
-    th.setAttribute("scope", "row");
-    th.appendChild(document.createTextNode(position++));
-
-    let td1 = document.createElement("td");
-    td1.appendChild(document.createTextNode(name));
-
-    let td2 = document.createElement("td");
-    td2.appendChild(document.createTextNode(amount));
-
-    tr.appendChild(th);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-  });
-}
+      // Clear existing leaderboard data
+      clearLeaderboard();
+      let position = 1;
+      console.log(res.data);
+      res.data.forEach((user) => {
+        let name = user.name;
+        let amount = user.totalExpenses;
+  
+        //console.log(name, amount);
+        let tr = document.createElement("tr");
+        tr.setAttribute("class", "trStyle");
+  
+        tbody.appendChild(tr);
+  
+        let th = document.createElement("th");
+        th.setAttribute("scope", "row");
+        th.appendChild(document.createTextNode(position++));
+  
+        let td1 = document.createElement("td");
+        td1.appendChild(document.createTextNode(name));
+  
+        let td2 = document.createElement("td");
+        td2.appendChild(document.createTextNode(amount));
+  
+        tr.appendChild(th);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+      });
+    } catch (error) {
+      console.error("Error fetching leaderboard data:", error);
+    }
+  }
+  
 
 document.addEventListener("DOMContentLoaded", getLeaderboard);
+
+function clearLeaderboard() {
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+  }
+  
